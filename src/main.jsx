70e3142f0f03bc3ec8701026eb591dbc89580a02
ahmedalaasobhyomar11-lab <ahmed.alaasobhyomar11@gmail.com>
2026-08-10 22:35:39 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Pulse from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Pulse />
+  </React.StrictMode>
+);
